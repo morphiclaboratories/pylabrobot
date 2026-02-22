9933bbbbfc547b44d4494f3f299e6ee00b7afbfb
cmoscy <46687103+cmoscy@users.noreply.github.com>
2026-02-21 17:33:04 -0800

    CPAC Slot

--- pylabrobot/temperature_controlling/inheco/cpac.py
+++ pylabrobot/temperature_controlling/inheco/cpac.py
@@ -3,6 +3,13 @@ from pylabrobot.temperature_controlling.inheco.control_box import InhecoTECContr
 from pylabrobot.temperature_controlling.inheco.cpac_backend import InhecoCPACBackend
 from pylabrobot.temperature_controlling.temperature_controller import TemperatureController
 
+# Dimensions (mm) and child location for Inheco CPAC Ultraflat 7000166, 7000190, 7000165.
+# https://www.inheco.com/data/pdf/cpac-brochure-1013-1032-34.pdf
+CPAC_SIZE_X = 113
+CPAC_SIZE_Y = 89
+CPAC_SIZE_Z = 129
+CPAC_CHILD_LOCATION = Coordinate(x=8, y=11, z=77)  # x from spec, y and z measured
+
 
 def inheco_cpac_ultraflat(
   name: str, control_box: InhecoTECControlBox, index: int
@@ -16,9 +23,9 @@ def inheco_cpac_ultraflat(
   return TemperatureController(
     name=name,
     backend=InhecoCPACBackend(control_box=control_box, index=index),
-    size_x=113,  # from spec
-    size_y=89,  # from spec
-    size_z=129,  # from spec
-    child_location=Coordinate(x=8, y=11, z=77),  # x from spec, y and z measured
+    size_x=CPAC_SIZE_X,
+    size_y=CPAC_SIZE_Y,
+    size_z=CPAC_SIZE_Z,
+    child_location=CPAC_CHILD_LOCATION,
     model=inheco_cpac_ultraflat.__name__,
   )
